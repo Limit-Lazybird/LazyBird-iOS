51e7fbf9b3e19332477748aa8b066c6fd47bf265
mumu <tnddls2ek@naver.com>
2021-12-06 00:50:56 +0900

    [UPDATE] custom 전시 정보 캘린더 저장 api 연결

---
+++ LazyBird/Sources/Models/Calendar/CustomInfoSaveRequest.swift
@@ -0,0 +1,12 @@
+//
+//  CustomInfoSaveRequest.swift
+//  LazyBird
+//
+//  Created by 이숭인 on 2021/12/05.
+//
+
+import UIKit
+
+class CustomInfoSaveRequest: NSObject {
+
+}
---
+++ LazyBird/Sources/ViewModels/Calendar/AddExhibitionScheduleViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  AddExhibitionScheduleViewModel.swift
+//  LazyBird
+//
+//  Created by 이숭인 on 2021/12/05.
+//
+
+import UIKit
+
+class AddExhibitionScheduleViewModel: NSObject {
+
+}
