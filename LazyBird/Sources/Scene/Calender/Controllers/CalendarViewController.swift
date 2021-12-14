47d7109957c2ce5042875303db4fc51ed006b83d
mumu <tnddls2ek@naver.com>
2021-12-14 18:32:04 +0900

    [REFACTOR] 캘린더에 등록되지 않은 전시 수 바인딩

--- LazyBird/Sources/Scene/Calender/Controllers/CalendarViewController.swift
+++ LazyBird/Sources/Scene/Calender/Controllers/CalendarViewController.swift
@@ -460,6 +460,7 @@ extension CalendarViewController: CalendarViewDelegate{
         //TODO: request
         self.viewModel.requestCustomScheduleDelete(exhbt_cd: schedule.exhbt_cd){
             self.viewModel.requestMonthlySchedules(reser_dt: self.dateFormatterForAPI.string(from: self.calender.currentPage))
+            self.viewModel.requestUnregistedSchedules() // 예약했지만 등록되지 않은 전시 리스트 불러오기
         }
     }
     
@@ -471,6 +472,7 @@ extension CalendarViewController: CalendarViewDelegate{
                                               end_time: nil)
         self.viewModel.requestBookedScheduleDelete(bookedSchedule: parameter){
             self.viewModel.requestMonthlySchedules(reser_dt: self.dateFormatterForAPI.string(from: self.calender.currentPage))
+            self.viewModel.requestUnregistedSchedules() // 예약했지만 등록되지 않은 전시 리스트 불러오기
         }
     }
 }
